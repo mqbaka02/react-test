b7ee10986675dd4a54f978d19162b07f70b8bc09
mqbaka\mqbaka <mqbaka@gmail.com>
2024-12-04 11:37:35 +0300

    Use of CreatePortal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import Input from "./components/forms/Input";
 import Checkbox from "./components/forms/Checkbox";
 
 import "./assets/main.css";
 import ProductCategoryRow from "./products/ProductCategoryRow";
 import ProductRow from "./products/ProductRow";
+import { createPortal } from "react-dom";
 
 const PRODUCTS = [  
   {category: "Fruits", price: "$1", stocked: true, name: "Apple"},  
@@ -16,11 +17,60 @@ const PRODUCTS = [
 ];
 
 function App() {
+  return <>
+    <div style={{width: 150, overflowY: 'scroll', background: '#EEE', padding: 20, position: 'relative', maxHeight: '100vh', boxSizing: 'border-box'}}>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam voluptatem, voluptas non ut illo, molestias, quaerat obcaecati officiis necessitatibus ad enim placeat! Repellat atque, quaerat maiores blanditiis laudantium fugiat doloribus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla voluptas similique quo officiis ducimus! Molestiae natus id rem deserunt sapiente mollitia qui blanditiis laudantium laborum excepturi, neque dolor voluptate ducimus!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum accusantium ducimus expedita molestiae eligendi laborum, in id odit tenetur facilis eius aperiam dolorum ut, debitis placeat minima. Modi, nemo odio!
+      </p>
+      <Modal/>
+    </div>
+  </>;
+}
+
+function Modal () {
+  return createPortal(
+    <div style={{position: 'absolute', top: 0, right: 0,padding: 10, border: 'solid 1px grey', background: '#FFF'}}>
+    Modal window.
+  </div>, document.body);
+  //the Modal element is a child of the div, but because of createPortal() it is now a child of the body.f
+}
+
+function UseMemoExample({processHeavyFunction}) {
+  const [firstname, setFirstname]= useState("john");
+  const [pass, setPass]= useState("01234");
+  const security= useMemo(()=> {
+    return processHeavyFunction(pass);//gets executed only when pass is changed. Usefull if the function is slow.
+  },[pass]); 
+  // const security= passwordSecurity(pass);
+
+  return <>
+    <div className="container vstack my-3 gap-2">
+      <Input label="User name" value={firstname} onChange={setFirstname}/>
+      <Input label="Pass" value={pass} onChange={setPass} type="password"/>
+      Security: {security};
+    </div>
+  </>
+}
+
+function passwordSecurity(someString) {
+  return someString.length;
+}
+
+function ListOfFruits({products}){
   const [showStockedOnly, setShowStockedOnly]= useState(false);
   const [searchedValue, setSearchedValue]= useState("");
 
-  const visibleProducts= PRODUCTS.filter(product=> {
+  const visibleProducts= products.filter(product=> {
     if(showStockedOnly && !product.stocked){
+      return false;
+    }
+    if(searchedValue && !product.name.includes(searchedValue)){
       return false;
     }
     return true;
@@ -29,7 +79,6 @@ function App() {
   return <div className="container my-3">
     <Searchbar showStockedOnly={showStockedOnly} onStockedOnlyChanged={setShowStockedOnly} searchedValue={searchedValue} searchHandler={setSearchedValue}/>
     <ProductTable products={visibleProducts}/>
-
   </div>;
 }
 
@@ -60,6 +109,7 @@ function ProductTable({products}) {
         <tr>
           <th>Name</th>
           <th>Price</th>
+          <th>Stock</th>
         </tr>
       </thead>
       <tbody>
--- src/products/ProductRow.jsx
+++ src/products/ProductRow.jsx
@@ -3,9 +3,10 @@
  * @param {{name: string, onStock: boolean, price: string}} product
  */
 export default function ProductRow({product}) {
-    const style= product.onStock ? undefined : {color: "#999"};
+    const style= product.stocked ? {color: "#000"} : {color: "#999"};
     return <tr>
         <td style={style}>{product.name}</td>
         <td>{product.price}</td>
+        <td>{product.stocked? "yes" : "no"}</td>
     </tr>;
 }
